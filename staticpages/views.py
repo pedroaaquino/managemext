a6977c5e72d90687bf0b2310f1940ed0e4a779ff
Pedro Aquino <pedrobiancoaquino@gmail.com>
2023-11-23 09:39:10 -0300

    Criar site estático com Django

--- staticpages/views.py
+++ staticpages/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("<h1>Hello World!</h1>")
+    context = {}
+    return render(request, 'staticpages/index.html', context)
